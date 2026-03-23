cc05123df19411f910d04bdf3b8d14c37791f00a
Maria Tomilova <m.tomilova2802@gmail.com>
2026-03-23 22:33:06 +0300

    First commit 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name,
+  this.gender = gender,
+  this.age = age,
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")){
+    this.marks.push(...marks)
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
+    return 0;
+  }
+  return this.marks.reduce((acc, cur) => acc + cur, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
